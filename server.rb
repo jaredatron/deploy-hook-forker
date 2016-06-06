137ad122e3d9d27791b79ccee03256511f29becd
Jared Grippe <jared@learnup.com>
2016-06-06 10:15:14 -0700

    upgraded ruby & added support for .env in development

--- server.rb
+++ server.rb
@@ -1,3 +1,8 @@
+ENV['RACK_ENV'] ||= 'development'
+
+require 'bundler'
+Bundler.require
+
 require 'sinatra'
 require 'yaml'
 require 'uri'
@@ -6,37 +11,48 @@ require 'erb'
 
 use Rack::Logger
 
-set :config_path, File.expand_path('../config.yml', __FILE__)
-set :config, ->{ YAML.load(ERB.new(File.read(config_path)).result) }
+configure :development do
+  require 'dotenv'
+  Dotenv.load
+end
+
+configure do
+  set :config_path, File.expand_path('../config.yml', __FILE__)
+  set :config, ->{ YAML.load(ERB.new(File.read(config_path)).result) }
+  set :secret, ENV["SECRET"]
+  puts "SECRET=#{ENV["SECRET"]}"
+  puts settings.config.inspect
+end
+
 
 helpers do
   def logger
     request.logger
   end
+
   def config
     settings.config[params['app']] || {}
   end
 end
 
-get '/' do
-  if ENV["SECRET"] && params.delete("secret") == ENV["SECRET"]
-    content_type :json
-    status 200
-    return JSON.pretty_generate(settings.config)
+before do
+  if settings.secret && params["secret"] != settings.secret
+    halt 401, "Unauthorized"
   end
+end
+
+get '/' do
+  content_type :json
   status 200
-  "ok"
+  JSON.pretty_generate(settings.config)
 end
 
 post '/' do
-  if ENV["SECRET"] && params.delete("secret") != ENV["SECRET"]
-    status 401
-    return
-  end
   logger.info "RECIEVED POST: #{params.inspect}"
   forwardable_params = params.dup
   forwardable_params.delete('splat')
   forwardable_params.delete('captures')
+  forwardable_params.delete('secret')
 
   config.values.each do |url|
     uri = URI.parse(url)
